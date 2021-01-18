c1eb1cbf59f94e73696cddc485bd0caa96669c01
ahmed nasser <ahmednas197@gmail.com>
2021-01-18 23:05:18 +0200

    update Defualt component

--- src/components/Default.js
+++ src/components/Default.js
@@ -3,8 +3,21 @@ import React, { Component } from "react";
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h3>Page not found</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+            <h1 className="dispaly-3">404</h1>
+            <h1>Error</h1>
+            <h2>Page not found</h2>
+            <p>
+              requsted url{" "}
+              <strong className="text-primary">
+                {this.props.location.pathname}
+              </strong>{" "}
+              was not found
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
